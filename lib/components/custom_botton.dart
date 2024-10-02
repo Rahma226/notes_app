df5dd27aebb22382e6a080bad03b20a63d7fa845
Rahma226 <sherifrahma829@gmail.com>
2024-10-02 21:34:49 +0300

    validate user input

--- lib/components/custom_botton.dart
+++ lib/components/custom_botton.dart
@@ -2,24 +2,29 @@ import 'package:flutter/material.dart';
 import 'package:notes_app/constant.dart';
 
 class CustomBotton extends StatelessWidget {
-  const CustomBotton({super.key});
+  const CustomBotton({super.key, this.onTap});
+
+ final void Function()? onTap;
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      width: MediaQuery.of(context).size.width,
-      height: 55,
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(16),
-        color: kcolor4,
-      ),
-      child: const Center(
-        child: Text(
-          'Add',
-          style: TextStyle(
-            color: Colors.black,
-            fontSize: 18,
-            fontWeight: FontWeight.w600,
+    return GestureDetector(
+      onTap: onTap,
+      child: Container(
+        width: MediaQuery.of(context).size.width,
+        height: 55,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(16),
+          color: kcolor4,
+        ),
+        child: const Center(
+          child: Text(
+            'Add',
+            style: TextStyle(
+              color: Colors.black,
+              fontSize: 18,
+              fontWeight: FontWeight.w600,
+            ),
           ),
         ),
       ),
--- lib/components/custom_text_field.dart
+++ lib/components/custom_text_field.dart
@@ -2,23 +2,29 @@ import 'package:flutter/material.dart';
 import 'package:notes_app/constant.dart';
 
 class CustomTextField extends StatelessWidget {
-  const CustomTextField({super.key, required this.hint, this.maxLines = 1});
+  const CustomTextField(
+      {super.key, required this.hint, this.maxLines = 1, this.onSaved});
 
   final String hint;
   final int maxLines;
 
+  final void Function(String?)? onSaved;
+
   @override
   Widget build(BuildContext context) {
-    return TextField(
+    return TextFormField(
+      onSaved: onSaved,
+      validator: (value) {
+        if (value?.isEmpty  ?? true){
+          return 'Field is required';
+        }
+      },
       cursorColor: kcolor4,
       maxLines: maxLines,
       decoration: InputDecoration(
         hintText: hint,
         hintStyle: const TextStyle(
-          fontWeight: FontWeight.normal,
-          fontFamily: 'Poppins',
-          fontSize: 12
-        ),
+            fontWeight: FontWeight.normal, fontFamily: 'Poppins', fontSize: 12),
         border: buildBorder(),
         enabledBorder: buildBorder(),
         focusedBorder: buildBorder(Kcolor2),
